5015d96e9c971af3d3fc6636fb40226e98aca087
Elijah1123 <elijahmzalendo659@gmail.com>
2025-08-27 21:56:02 +0300

    python

--- lib/anagram.py
+++ lib/anagram.py
@@ -9,3 +9,10 @@ class Anagram:
             if candidate.lower() != self.word and sorted(candidate.lower()) == self.sorted_word:
                 matches.append(candidate)
         return matches
+
+
+if __name__ == "__main__":
+    detector = Anagram("listen")
+    words = ["enlists", "google", "inlets", "banana", "Silent"]
+
+    print("Anagrams found:", detector.match(words))
